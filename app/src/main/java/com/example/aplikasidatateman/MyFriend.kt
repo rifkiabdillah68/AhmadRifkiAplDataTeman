4cec48354dcebd75b716d41d273ef127769f59a8
rifkiabdillah68 <rifkiabdillah68@gmail.com>
2021-04-30 00:27:04 +0700

    Aplikasi Data Teman
    Aplikasi Data Teman menggunakan ViewPager2, Fragment dan TabLayout

---
+++ app/src/main/java/com/example/aplikasidatateman/MyFriend.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasidatateman
+
+class MyFriend (
+    val nama:String,
+    val jkel:String,
+    val email:String,
+    val telp:String,
+    val alamat:String
+)
--- app/src/main/java/com/example/aplikasidatateman/MyFriendsFragment.kt
+++ app/src/main/java/com/example/aplikasidatateman/MyFriendsFragment.kt
@@ -6,8 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_my_friends.*
 
 class MyFriendsFragment : Fragment() {
+    lateinit var listTeman:ArrayList<MyFriend>
+
+    private fun simulasiDataTeman(){
+        listTeman= ArrayList()
+        listTeman.add(MyFriend("Ahmad Rifki Abdillah", jkel = "Laki-laki", email = "rifki@me.com", telp = "098308754987", alamat = "Malang"))
+        listTeman.add(MyFriend(nama = "Dhiya Ulhaq Dazakiyyah", jkel = "Laki-laki", email = "ulhaq@you.com", telp = "9834793874", alamat = "Blitar"))
+        listTeman.add(MyFriend(nama = "Oky MIkhael", jkel = "Laki-laki", email = "oky@you.com", telp = "0819834793874", alamat = "Tulungagung"))
+        listTeman.add(MyFriend(nama = "Widya Retnaning Pratiwi", jkel = "Perempuan", email = "widya@you.com", telp = "0859834793874", alamat = "Kediri"))
+        listTeman.add(MyFriend(nama = "Indra Oky Sandi", jkel = "Laki-laki", email = "indra@you.com", telp = "02134793874", alamat = "Jombang"))
+        listTeman.add(MyFriend(nama = "I Komang Damai Armawan", jkel = "Laki-laki", email = "komang@you.com", telp = "088834793874", alamat = "Mojokerto"))
+        listTeman.add(MyFriend(nama = "Daud Ridho Ilahi", jkel = "Laki-laki", email = "daud@you.com", telp = "082834793874", alamat = "Batu"))
+    }
+
+    private fun tampilTeman(){
+        rv_listMyfriends.layoutManager=LinearLayoutManager(activity)
+        rv_listMyfriends.adapter=MyFriendAdapter(activity!!, listTeman)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,7 +43,16 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
     }
 
+    private fun initView(){
+        simulasiDataTeman()
+        tampilTeman()
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
 }
